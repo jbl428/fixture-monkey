f09a56876fa6df0cdacc8a9a69b23fc1ff84eb03
jbl428 <jbl428@users.noreply.github.com>
2023-07-09 04:42:36 +0000

    deploy: 3ce3135842e99338c9c4ed0f7d2d3350a23a6ab1

--- js/tabpane-persist.js
+++ js/tabpane-persist.js
@@ -2,17 +2,19 @@ if (typeof Storage !== 'undefined') {
     const activeLanguage = localStorage.getItem('active_language');
     if (activeLanguage) {
         document
-            .querySelectorAll('.tab-' + activeLanguage)
+            .querySelectorAll('.persistLang-' + activeLanguage)
             .forEach((element) => {
-                element.click();
+                new bootstrap.Tab(element).show();
             });
     }
 }
-function handleClick(language) {
+function persistLang(language) {
+    console.log("Klicked persistlang");
     if (typeof Storage !== 'undefined') {
         localStorage.setItem('active_language', language);
-        document.querySelectorAll('.tab-' + language).forEach((element) => {
-            element.click();
+        document.querySelectorAll('.persistLang-' + language)
+            .forEach((element) => {
+                new bootstrap.Tab(element).show();
         });
     }
 }
